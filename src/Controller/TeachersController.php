6c16eeb5e2b7770c2b6ddf3390a5379ade8f84d2
Zorakiel <cecambon@gmail.com>
2020-02-24 15:48:25 +0100

    Creation of teacher form in teachers page

--- src/Controller/TeachersController.php
+++ src/Controller/TeachersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Form\TeacherType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,31 +15,14 @@ class TeachersController extends AbstractController
      */
     public function renderHomepage()
     {
-        $teachers = $this->callGET("enseignants");
+        $teachers = RestAPI::callGET("enseignants");
+        $teacher = RestAPI::callGET("enseignants/2001-A001", "App\Entity\Enseignant");
+
+        $teacherForm = $this->createForm(TeacherType::class, $teacher);
 
         return $this->render('teachers.html.twig', [
-            'teachers' => $teachers
+            'teachers' => $teachers,
+            'teacherForm' => $teacherForm->createView()
         ]);
-    }
-
-    private function createSchoolYearOptions()
-    {
-        $schoolYears = callGET("annees");
-
-        foreach ($schoolYears as $schoolYear)
-        {
-            echo '<option value="' . $schoolYear->idAnnee . '">' . $schoolYear->value . '</option>';
-        }
-    }
-
-    private function callGET($url)
-    {
-        $client = curl_init("localhost:8080/" . $url);
-
-        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
-
-        $response = curl_exec($client);
-
-        return json_decode($response);
     }
 }
