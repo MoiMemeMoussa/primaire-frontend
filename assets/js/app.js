a2e17c64cac0cde27b594bf61cf6a7c5ad387617
Zorakiel <cecambon@gmail.com>
2020-02-25 11:52:34 +0100

    Added a Bootstrap theme to the navigation menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import '../css/sb-admin-2.min.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
