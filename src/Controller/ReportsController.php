0f7b00a6a65af557d22db79184c9a9d309aa3adf
Zorakiel <cecambon@gmail.com>
2020-03-02 10:51:58 +0100

    Revert "Changed fields names in createSQLdata() function"
    This reverts commit bfbc27d05c7426b39db1330fbeeeb0002c610485.

--- src/Controller/ReportsController.php
+++ src/Controller/ReportsController.php
@@ -20,24 +20,24 @@ class ReportsController extends AbstractController
 
     private function createSQLdata()
     {
-        $this->post("annees", '{"id_annee":1,"value":"2016-2017"}');
-        $this->post("annees", '{"id_annee":2,"value":"2017-2018"}');
-        $this->post("annees", '{"id_annee":3,"value":"2018-2019"}');
+        $this->post("annees", '{"idAnnee":1,"value":"2016-2017"}');
+        $this->post("annees", '{"idAnnee":2,"value":"2017-2018"}');
+        $this->post("annees", '{"idAnnee":3,"value":"2018-2019"}');
 
-        $this->post("classes", '{"id_classe":4,"name":"CP"}');
-        $this->post("classes", '{"id_classe":5,"name":"CE1"}');
-        $this->post("classes", '{"id_classe":6,"name":"CE2"}');
-        $this->post("classes", '{"id_classe":7,"name":"CM1"}');
-        $this->post("classes", '{"id_classe":8,"name":"CM2"}');
-        $this->post("classes", '{"id_classe":9,"name":"6ème"}');
+        $this->post("classes", '{"idClasse":4,"name":"CP"}');
+        $this->post("classes", '{"idClasse":5,"name":"CE1"}');
+        $this->post("classes", '{"idClasse":6,"name":"CE2"}');
+        $this->post("classes", '{"idClasse":7,"name":"CM1"}');
+        $this->post("classes", '{"idClasse":8,"name":"CM2"}');
+        $this->post("classes", '{"idClasse":9,"name":"6ème"}');
 
-        $this->post("matieres", '{"id_matiere":10,"name":"Comptabilité"}');
-        $this->post("matieres", '{"id_matiere":11,"name":"Philosophie"}');
-        $this->post("matieres", '{"id_matiere":12,"name":"Anglais"}');
+        $this->post("matieres", '{"idMatiere":10,"name":"Comptabilité"}');
+        $this->post("matieres", '{"idMatiere":11,"name":"Philosophie"}');
+        $this->post("matieres", '{"idMatiere":12,"name":"Anglais"}');
 
-        $this->post("enseignants", '{"matricule":"2020-A001","first_name":"Shogo","last_name":"MAKISHIMA","title":"M","phone":915161718}');
+        $this->post("enseignants", '{"matricule":"2020-A001","firstName":"Shogo","lastName":"MAKISHIMA","title":"M","phone":915161718}');
 
-        $this->post("eleves", '{"id_eleve":13,"first_name":"Light","last_name":"YAGAMI","gender":"MASCULIN","father":"Ulquiora","mother":"Rukia","birth_date":"2000-05-16","place":"Tokyo"}');
+        $this->post("eleves", '{"idEleve":13,"firstName":"Light","lastName":"YAGAMI","gender":"MASCULIN","father":"Ulquiora","mother":"Rukia","birthDate":"2000-05-16","place":"Tokyo"}');
 
         dump("Données créées");
     }
