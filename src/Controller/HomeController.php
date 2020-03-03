8c317ac1004898b21b56bfcc34e5add0b3694c25
Zorakiel <cecambon@gmail.com>
2020-03-03 15:50:23 +0100

    Removed new year page content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Unirest;
 
 class HomeController extends AbstractController
 {
@@ -14,14 +13,6 @@ class HomeController extends AbstractController
      */
     public function renderHomepage(Request $request)
     {
-        $years = Unirest\Request::get("http://localhost:8080/annees")->body;
-        $classes = Unirest\Request::get("http://localhost:8080/annees/1/classes")->body;
-        $teacher = Unirest\Request::get("http://localhost:8080/classes/1/annees/1/enseignant")->body;
-
-        return $this->render('index.html.twig', [
-            'years' => $years,
-            'classes' => $classes,
-            'teacher' => $teacher
-        ]);
+        return $this->render('index.html.twig', []);
     }
 }
